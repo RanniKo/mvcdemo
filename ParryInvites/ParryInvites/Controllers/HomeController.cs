3496e04f19303b9821a19671de6dcee842b91d39
RanniKo <ranniko@naver.com>
2018-09-07 22:03:46 +0900

    commit test files

---
+++ ParryInvites/ParryInvites/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using ParryInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ParryInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse pResponse)
+        {
+            if(ModelState.IsValid)
+            {
+                return View("Thanks", pResponse);
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+    }
+}
