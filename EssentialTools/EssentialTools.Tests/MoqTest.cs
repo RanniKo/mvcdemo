8104b4d2f45077fba994d9791a81b512d2c81512
RanniKo <ranniko@naver.com>
2018-09-10 18:32:59 +0900

    ninject, moq test source

---
+++ EssentialTools/EssentialTools.Tests/MoqTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using EssentialTools.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace EssentialTools.Tests
+{
+    /// <summary>
+    /// http://code.google.com/p/moq/wiki/QuickStart
+    /// </summary>
+    [TestClass]
+    public class MoqTest
+    {
+        private Product[] products = {
+            new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+            new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+            new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+            new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+        };
+
+        [TestMethod]
+        public void Sum_Products_Correctly()
+        {
+            /*
+             Moq를 활용하면 Models 폴더에 만들어진 IDiscountHelper와 무관하게 오직 LinqValueCalculator 개체의 동작만 점검 가능
+             */
+            // arrange
+            Mock<IDiscountHelper> mock = new Mock<IDiscountHelper>();
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>()))
+                .Returns<decimal>(total => total);
+            var target = new LinqValueCalculator(mock.Object);
+
+            // act
+            var result = target.ValueProducts(products);
+
+            // assert
+            Assert.AreEqual(products.Sum(e => e.Price), result);
+        }
+
+
+        private Product[] CreateProduct(decimal value)
+        {
+            return new[] { new Product { Price = value } };
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Pass_Through_Variable_Discounts()
+        {
+            // arrange
+            Mock<IDiscountHelper> mock = new Mock<IDiscountHelper>();
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>()))
+                .Returns<decimal>(total => total);
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v == 0)))
+                .Throws<ArgumentOutOfRangeException>();//형식 매개변수로 지정한 예외의 새로운 인스턴스를 던지도록 Moq에 지시
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v > 100)))
+                .Returns<decimal>(total => (total * 0.9M));
+            mock.Setup(m => m.ApplyDiscount(It.IsInRange<decimal>(10, 100,
+                Range.Inclusive))).Returns<decimal>(total => total - 5);
+            var target = new LinqValueCalculator(mock.Object);
+
+            // act
+            decimal FiveDollarDiscount = target.ValueProducts(CreateProduct(5));
+            decimal TenDollarDiscount = target.ValueProducts(CreateProduct(10));
+            decimal FiftyDollarDiscount = target.ValueProducts(CreateProduct(50));
+            decimal HundredDollarDiscount = target.ValueProducts(CreateProduct(100));
+            decimal FiveHundredDollarDiscount = target.ValueProducts(CreateProduct(500));
+
+            // assert
+            Assert.AreEqual(5, FiveDollarDiscount, "$5 Fail");
+            Assert.AreEqual(5, TenDollarDiscount, "$10 Fail");
+            Assert.AreEqual(45, FiftyDollarDiscount, "$50 Fail");
+            Assert.AreEqual(95, HundredDollarDiscount, "$100 Fail");
+            Assert.AreEqual(450, FiveHundredDollarDiscount, "$500 Fail");
+            target.ValueProducts(CreateProduct(0));
+        }
+    }
+}
